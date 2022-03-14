b617b4cb5e1139b41813f97a036f10df15d4aafe
lindalice <73908773+lindalice@users.noreply.github.com>
2022-03-14 13:55:26 +0200

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -16,13 +16,13 @@ public class Calculator {
 		System.out.println(df.format(x1) + " - " + df.format(y1) + " = " + df.format(sub));
 	}
 	static void mul(double x1, double y1) {
-		double sum = x1 * y1;
+		double mul = x1 * y1;
 
 		System.out.println(df.format(x1) + " * " + df.format(y1) + " = " + df.format(mul);
 	}
 
 	static void div(double x1, double y1) {
-		double sum = x1 / y1;
+		double div = x1 / y1;
 
 		System.out.println(df.format(x1) + " / " + df.format(y1) + " = " + df.format(div));
 	}
